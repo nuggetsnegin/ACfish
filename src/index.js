8802b982c8e34e840d31aec7d26714a1d505601b
Negin <nuggetnegin@gmail.com>
2020-02-16 14:48:09 -0500

    created first class component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-// let's go!
+import React from 'react'; //behind the scenes goes into node modules folder
+//need a secondary package known as React DOM so we can render to the DOM:
+import { render } from 'react-dom'; //only need 1 method, cherry pick a method from the package we only need the Render method
 
-alert('hey')
+//every component we have will be its own class
+class StorePicker extends React.Component{
+    //needs atleast 1 method inside of it - rendder: what dom elements do I put on the page?
+    render(){
+        return <p>Wassap</p>
+    }
+}
+
+//only time where we do touch the DOM and thats when we mount it to the page
+render(<StorePicker />, document.querySelector('#main')); //need to give it an actual DOM element to mount too
