86ae70046f7b42d1245fe868d84629ffec7bc66b
Negin <nuggetnegin@gmail.com>
2020-02-16 14:51:48 -0500

    move StorePicker to its own component file

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'; 
+
+//every component we have will be its own class
+//needs atleast 1 method inside of it - rendder: what dom elements do I put on the page?
+class StorePicker extends Component {
+    render(){
+        return <p>Wassap</p>
+    }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'; //behind the scenes goes into node modules folder
 //need a secondary package known as React DOM so we can render to the DOM:
 import { render } from 'react-dom'; //only need 1 method, cherry pick a method from the package we only need the Render method
-
-//every component we have will be its own class
-class StorePicker extends React.Component{
-    //needs atleast 1 method inside of it - rendder: what dom elements do I put on the page?
-    render(){
-        return <p>Wassap</p>
-    }
-}
+import StorePicker from './components/StorePicker';
 
 //only time where we do touch the DOM and thats when we mount it to the page
 render(<StorePicker />, document.querySelector('#main')); //need to give it an actual DOM element to mount too
