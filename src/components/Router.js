810bd1ff34376ad357148aef5c1c4bac7f794cd7
Negin <nuggetnegin@gmail.com>
2020-02-16 15:59:39 -0500

    smol type and forgot to export defualt and import storepicker

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import StorePicker from './storePicker';
 
 const Router = () => {
   <BrowserRouter>
     <Switch>
       {/* tries the first route and uses fallbacks if not found*/}
-      <Route>
-        {/*what path - in this context its abs of / and renders out component storepicker*/}
-        <Route exact path="/" component={StorePicker} />
-      </Route>
+      {/*what path - in this context its abs of / and renders out component storepicker*/}
+      <Route exact path="/" component={StorePicker} />
     </Switch>
   </BrowserRouter>;
 };
+
+export default Router;
