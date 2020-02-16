9a054972e1ebf697e590639adab878cd9aa0d587
Negin <nuggetnegin@gmail.com>
2020-02-16 14:59:37 -0500

    why did I never know about React.Fragments before.....

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,7 +5,16 @@ import { render } from 'react-dom';
 //needs atleast 1 method inside of it - rendder: what dom elements do I put on the page?
 class StorePicker extends Component {
     render(){
-        return <p>Wassap</p>
+        return( //only ever return ONE element, you can put as many child elements inside a parent element, cannot return sibling elements
+            //the solution to that - wrap them in a React.Fragment tag!
+            <React.Fragment>
+                { /* no regular html comments have to use squigglies :'( */}
+                <p>fish</p>
+                <form className="store-selector">
+                    <h1>Please enter a store</h1>
+                </form>
+            </React.Fragment>
+        )
     }
 }
 
