b22aabcbf9a75be73c27d18dfb8520cc8bb3a6d9
Negin <nuggetnegin@gmail.com>
2020-02-16 16:00:21 -0500

    oops used squigglies instead of smoothies on stateless arrow function

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import StorePicker from './storePicker';
 
-const Router = () => {
+const Router = () => (
   <BrowserRouter>
     <Switch>
       {/* tries the first route and uses fallbacks if not found*/}
       {/*what path - in this context its abs of / and renders out component storepicker*/}
       <Route exact path="/" component={StorePicker} />
     </Switch>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
 export default Router;
