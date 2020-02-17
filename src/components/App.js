c85a461c015f7c205665c211b9dc283e50309889
Negin <nuggetnegin@gmail.com>
2020-02-17 11:35:49 -0500

    animal crossingfied smore

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends Component {
   //setting state with a property
@@ -36,7 +37,9 @@ class App extends Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Only the best pfffffppt" age={12} cool={true} />
-          {/*have to use squigglies for numbers and booleans*/}
+          <ul className="fishes">
+            <Fish />
+          </ul>
         </div>
         <Order />
         <Inventory
---
+++ src/components/Fish.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Fish extends Component {
+  render() {
+    return <div className="single-fish">🐟</div>;
+  }
+}
+
+export default Fish;
