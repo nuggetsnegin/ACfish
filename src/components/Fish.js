55a0530df9c3ed1f0be3eed46f296e0f71f421d3
Negin <nuggetnegin@gmail.com>
2020-02-17 11:55:28 -0500

    destructured props

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Fish extends Component {
   render() {
+    const { image, name } = this.props.details;
     return (
       <li className="menu-fish">
-        <img src={this.props.details.image} alt={this.props.details.name} />
-        <h3 className="fish-name">{this.props.details.name}</h3>
+        <img src={image} alt={name} />
+        <h3 className="fish-name">{name}</h3>
       </li>
     );
   }
