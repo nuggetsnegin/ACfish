f37c3c667d1f344a1c8e204ca1dcd3e758d3fe42
Negin <nuggetnegin@gmail.com>
2020-02-17 12:50:20 -0500

    Updates

---
+++ precache-manifest.4a56aabdcf98500681299366d6cce275.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fb456af1a9385a996b39964c9d12f7",
+    "url": "/ACfish/index.html"
+  },
+  {
+    "revision": "dfa04d408bbc93b7db41",
+    "url": "/ACfish/static/css/main.4ccaf625.chunk.css"
+  },
+  {
+    "revision": "105656a964d1954474cd",
+    "url": "/ACfish/static/js/2.230d4d2d.chunk.js"
+  },
+  {
+    "revision": "e76927ded4e98c8bb3c1b6b2d3f786e8",
+    "url": "/ACfish/static/js/2.230d4d2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfa04d408bbc93b7db41",
+    "url": "/ACfish/static/js/main.97af0d82.chunk.js"
+  },
+  {
+    "revision": "b993da6b3f3a0e0507aa",
+    "url": "/ACfish/static/js/runtime-main.4c430fe7.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/ACfish/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/ACfish/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/ACfish/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/ACfish/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/ACfish/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/ACfish/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/ACfish/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/ACfish/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/ACfish/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "9034030523d43be14291f3d21b599d58",
+    "url": "/ACfish/static/media/pattern.90340305.png"
+  }
+]);
